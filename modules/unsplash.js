0a634588a3757e1bed7fd85bb2f119cfb6e0c1e0
Maja Gedda <maja.gedda@zocom.se>
2023-02-06 11:17:06 +0100

    random url done

--- modules/unsplash.js
+++ modules/unsplash.js
@@ -1,12 +1,15 @@
 let client_id = 'V2_qc3wlqzic140eJ_PsKoIoOm6TmRueFrGzdcKqbXU';
 let startURL = 'https://api.unsplash.com/';
-let searchEndPoint = 'search/photos';
-let randomEndPoint = 'photos/random';
+let searchEndPoint = 'search/photos?query=';
+let randomEndPoint = 'photos/random?content_filter=high';
 
 function getImages(search, input) {
     if (search) {
-        let url = startURL + searchEndPoint + '?query=' + input + '&client_id=' + client_id;
+        let url = startURL + searchEndPoint + input + '&client_id=' + client_id;
         console.log(url);
+    } else {
+        let url = startURL + randomEndPoint + '&client_id=' + client_id;
+        console.log(url)
     }
 }
-console.log(getImages(true, 'puppies'));
+getImages(false);
